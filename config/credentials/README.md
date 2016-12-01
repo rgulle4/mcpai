a44683b3560155a0904da66a17e3ebc20238ec66
rgulle4 <rgulle4@lsu.edu>
2016-12-01 01:01:56 -0600

    Add some key management (still not scure)

---
+++ config/credentials/README.md
@@ -0,0 +1,5 @@
+# credentials
+
+This directory MUST have a file called `google.txt`, with at least
+one line, and in each line, the first word is a Google api key.
+
--- src/helpers/Helper.java
+++ src/helpers/Helper.java
@@ -5,24 +5,78 @@ import com.google.gson.GsonBuilder;
 import com.google.maps.GeoApiContext;
 import mapUtils.StraightLineDistance;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public final class Helper {
 
     public static boolean DEBUG_MODE = true;
 
-    public static final String GOOGLE_API_KEY
-          = "AIzaSyCVcA7zkndeuPwvejRsbtngKfC-H40Gq8Y";
-//          = "AIzaSyCxC53nAkMaeEDWFCmCT8JXOA8Qcv8ej0g";
-//          = System.getenv("MCPAIGOOGLEAPIKEY");
+    /* ---------------------------------------------------- */
+    
+    public static String GOOGLE_API_KEY;
+    
+    private static final String CREDENTIALS_PATH
+          = "config/credentials/";
+    private static final String KEYS_FILE_NAME
+          = "google.txt";
+    private static final String KEYS_FILE_PATH
+          = CREDENTIALS_PATH + KEYS_FILE_NAME;
+    private static final File KEYS_FILE
+          = new File(KEYS_FILE_PATH);
+    
+    private static Scanner fileScanner;
+    private static Scanner lineScanner;
+    
+    /** get a particular api key */
+    public static String getApiKey(int n) {
+        try {
+            int i = 0;
+            fileScanner = new Scanner(KEYS_FILE);
+            while (i <= n && fileScanner.hasNextLine()) {
+                if (i != n) {
+                    fileScanner.nextLine();
+                } else {
+                    lineScanner = new Scanner(fileScanner.nextLine());
+                    GOOGLE_API_KEY = lineScanner.next();
+                    lineScanner.close();
+                    fileScanner.close();
+                    return GOOGLE_API_KEY;
+                }
+                i++;
+            }
+            lineScanner.close();
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.err.println("ERROR: cant getApiKey(" + n + ")");
+        return GOOGLE_API_KEY;
+    }
+    
+    /** get the last used api key, or first key if not yet set */
+    public static String getApiKey() {
+        if (GOOGLE_API_KEY == null)
+            GOOGLE_API_KEY = getApiKey(0);
+        return GOOGLE_API_KEY;
+    }
+    
+    /* ---------------------------------------------------- */
 
-    public static final GeoApiContext
-          GEO_API_CONTEXT = new GeoApiContext().setApiKey(GOOGLE_API_KEY);
-
+    public static final GeoApiContext GEO_API_CONTEXT
+          = new GeoApiContext().setApiKey(GOOGLE_API_KEY);
     public static final Gson GSON_PP
           = new GsonBuilder().setPrettyPrinting().create();
     public static final Gson GSON
           = new Gson();
+
+    /* ---------------------------------------------------- */
+
+    public static final StraightLineDistance SLD
+          = new StraightLineDistance();
     
-    public static final StraightLineDistance SLD = new StraightLineDistance();
+    /* ---------------------------------------------------- */
     
     public static void printObject(Object o) {
         System.out.print(o + ": ");
@@ -41,7 +95,9 @@ public final class Helper {
     public static String toJsonPretty(Object o) {
         return GSON_PP.toJson(o);
     }
-
+    
+    /* ---------------------------------------------------- */
+    
     public static void printlnDebug(Object o) {
         if (DEBUG_MODE) System.out.println(o);
     }
@@ -49,6 +105,8 @@ public final class Helper {
     public static void printDebug(Object o) {
         if (DEBUG_MODE) System.out.print(o);
     }
+    
+    /* ---------------------------------------------------- */
 
     public static void println(Object o) {
         System.out.println(o);
@@ -60,5 +118,9 @@ public final class Helper {
 
     public static void print(Object o) {
         System.out.print(o);
+    }
+    
+    public static void printError(Object o) {
+        System.err.println(o);
     }
 }
