c01a8396099401f9850e5fdb45e7491446ff45b0
rgulle4 <rgulle4@lsu.edu>
2016-12-01 09:42:54 -0600

    Clean up Geocoder

--- src/mapUtils/Geocoder.java
+++ src/mapUtils/Geocoder.java
@@ -15,16 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Get coordinates, formatted address, and maybe other info from Google
- *    In the response, we probably just want - results[i].formatted_address
- *                                           - results[i].geometry.location
- *     Pls change the places.Location class as necessary also.
- *
- *     https://gist.github.com/rgulle4/7cdf66ce833d14431dbf926c7a91dbbf
- *     https://maps.googleapis.com/maps/api/geocode/json?&address=baton%20rouge%2C%20LA
- *
- *     https://gist.github.com/rgulle4/df6665dde76b8c779cf799d380e021ca
- *     https://maps.googleapis.com/maps/api/geocode/json?&address=baton%20rouge%2C%20LA
+ * Geocodes a Location object.
  */
 public final class Geocoder {
     private static String GOOGLE_API_KEY = Helper.getApiKey();
@@ -32,10 +23,6 @@ public final class Geocoder {
 
     private static final GeoApiContext
           geoApiContext = Helper.GEO_API_CONTEXT;
-    
-    public static void main(String[] args) {
-        
-    }
     
     /**
      * Geocodes a location, ie finds its latlng.
