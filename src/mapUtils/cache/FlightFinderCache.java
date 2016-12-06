aa1212fb7968c140128ee37aa3ba366d4d7c3bd6
rgulle4 <rgulle4@lsu.edu>
2016-12-06 12:44:16 -0600

    Add Vincent's FlightFinderCache

---
+++ src/mapUtils/cache/FlightFinderCache.java
@@ -0,0 +1,88 @@
+package mapUtils.cache;
+
+import java.sql.*;
+import java.time.LocalTime;
+
+public final class FlightFinderCache {
+    private static FlightFinderCache instance = null;
+    public static final String DB_FILE_NAME = "apicache.db";
+
+    public static String dbFileName;
+    private static Connection conn;
+    private static Statement stmt;
+    private static ResultSet rs;
+
+    public static FlightFinderCache getInstance() {
+        if (instance == null)
+            instance = new FlightFinderCache(DB_FILE_NAME);
+        return instance;
+    }
+
+    private FlightFinderCache() {
+        this(DB_FILE_NAME);
+    }
+
+    private FlightFinderCache(String dbFileName) {
+        this.dbFileName = dbFileName;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:sqlite:" + this.dbFileName);
+        } catch (SQLException e) { e.printStackTrace(); }
+    }
+
+    /*
+     *    CREATE TABLE road_distance_cache (
+     *        origin_string NOT NULL,
+     *        destination_string NOT NULL,
+     *        response_json,
+     *
+     *        PRIMARY KEY (origin_string, destination_string)
+     *    );
+     */
+
+    public static String get(String origin, String destination) {
+        String sqlSelect = (new StringBuilder("SELECT response_json "))
+                .append("FROM flight_finder_cache ")
+                .append("WHERE origin_string=\'")
+                .append(origin)
+                .append("\' ")
+                .append("AND destination_string=\'")
+                .append(destination)
+                .append("\'")
+                .append("AND current_date=\'")
+                .append(Time.valueOf(LocalTime.now()))
+                .append("\';")
+                .toString();
+
+        String responseJson = null;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sqlSelect);
+            if (!rs.next())
+                return null;
+            responseJson = rs.getString("response_json");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(responseJson);
+        return responseJson;
+    }
+
+    public static void put(String origin, String destination, String responseJson)
+    {
+        String sqlString = (new StringBuilder("INSERT INTO flight_finder_cache "))
+                .append("(origin_string, destination_string, response_json) ")
+                .append("VALUES (")
+                .append("\'").append(origin).append("\', ")
+                .append("\'").append(destination).append("\', ")
+                .append("\'").append(responseJson).append("\'")
+                .append(");").toString();
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sqlString);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
