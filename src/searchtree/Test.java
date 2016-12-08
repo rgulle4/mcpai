bebfd98f91ba0172f1189b9dcec32bcc31082a41
rgulle4 <rgulle4@lsu.edu>
2016-12-07 22:23:54 -0600

    Show search by TIME vs PRICE results.
    Location a = new Location("Amite, LA");
    Location b = new Location("Hayward, CA");
    Search s1 = new Search(a, b);
    
    s1.setLimit(900); // dollars
    s1.uniformSearch(Search.CostType.TIME);
    
    // =>
    // ----------------------
    // Time: 6.392222222222222
    // Price: 530.3890315284527
    // Hayward, CA
    // SFO
    // MSY
    // Amite, LA
    // ----------------------
    
    s1.setLimit(900); // hours
    s1.uniformSearch(Search.CostType.PRICE);
    
    // =>
    // ----------------------
    // Time: 31.408055555555556
    // Price: 286.94826184292054
    // Hayward, CA
    // Amite, LA
    // ----------------------

--- src/searchtree/Test.java
+++ src/searchtree/Test.java
@@ -7,28 +7,28 @@ public class Test {
         Location a = new Location("Amite, LA");
         Location b = new Location("Hayward, CA");
         Search s1 = new Search(a, b);
-        s1.setLimit(900);
+        
+        s1.setLimit(900); // dollars
         s1.uniformSearch(Search.CostType.TIME);
         
         // =>
         // ----------------------
-        // Time: 9.223611111111111
-        // Price: 370.0789399026043
+        // Time: 6.392222222222222
+        // Price: 530.3890315284527
         // Hayward, CA
-        // SJC
-        // GPT
+        // SFO
+        // MSY
         // Amite, LA
         // ----------------------
-
+    
+        s1.setLimit(900); // hours
         s1.uniformSearch(Search.CostType.PRICE);
     
         // =>
         // ----------------------
-        // Time: 9.223611111111111
-        // Price: 370.0789399026043
+        // Time: 31.408055555555556
+        // Price: 286.94826184292054
         // Hayward, CA
-        // SJC
-        // GPT
         // Amite, LA
         // ----------------------
     }
